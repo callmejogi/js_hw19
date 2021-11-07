4fda842165ba855d1fa6f4d6cb73b735fcf6d740
callmejogi <kutafin01@gmail.com>
2021-11-07 19:37:21 +0200

    done

--- js/script.js
+++ js/script.js
@@ -33,22 +33,36 @@
 
 // console.log(object.js[0]);
 
-// #5 & #6
+// #5 
 
-// const arr = ['x','xx','xxx','xxxx','xxxxx','xxxxxx'];
-
-// function arrayFill(num){
-//     arr = [];
+// function arrayFill(num = 5){
+//     const arr = [];
+//     let symb = 'x';
 //     for(let i = 0; i < num;i++){
-//         arr.unshift('x'),
-//         arr.push('x');
+//         arr.push(symb);
+//         symb += symb[0];
+
 //     }
-//     console.log(arr);
+//     return arr;
 // }
-// arrayFill(5);
+// console.log(arrayFill());
 
+//Task 6
+// function arrayFill(num){
+//     const arr = [];
 
-// const array = [1,22,333,4444,55555];
+//     for(let i = 1; i < num ;i++){
+//         let symb = '';
+//         for(let j = 0; j < i; j++){
+//             symb += i;
+//         }
+        
+//         arr.push(symb)
+//     }
+//     return arr;
+// }
+// console.log(arrayFill(10));
+
 
 // №7
 
@@ -64,16 +78,90 @@
 // arrayFill('x',5);
 
 // #8
-const array = [7,3,4,5,6,10,12,0]
 
-const result = array.reduce(function(accum,item,index,array){
-    arr = [];
-    const sum = accum + item;
-    for(let i = 0;i < array.length;i++){
-        if (accum < 10) arr.push(item);
-    }
+// const array = [9,3,4,5,6,10,12,0];
 
-    return sum
-})
-console.log(arr.length)
-console.log(result);
+// Vol.1
+// function getSumFirstElems(arr){
+//     let sum = 0;
+//     let result = 0;
+//     for (let i = 0 ; i < array.length; i++){
+//         if(sum > 10) break;
+//         sum += array[i];
+//         result += 1; 
+//     }
+//     return result;
+// }
+
+// console.log(getSumFirstElems(array));
+
+// vol.2
+// let a = array.reduce((accum,item) => {
+//     if(accum.sum > 10) return accum;
+
+//     accum.sum += item;
+//     accum.result += 1;
+//     return accum;
+// },{sum:0,result:0}).result;
+
+// console.log(a);
+
+// Task 9
+
+// const arr9 = [1,5,3,7,8,9,11,2];
+
+// function inplaceReverse(arr) {
+//     for (let i = 0; i < arr.length - 1;i++) {
+//       arr.splice(i, 0, arr.pop());
+//     }
+//     return arr;
+//   }
+  
+// console.log(inplaceReverse(arr9));
+
+// вариант решения в новом массиве,чтоб не изменять изначальный
+// let getReversedArr = (array) => {
+//     const arr = [];
+//     for(let i = arr9.length - 1; i > 0;i--){
+//         arr.push(array[i]);
+//     }
+//     return arr;
+// };
+
+// console.log(getReversedArr(arr9));
+
+
+// Task10
+
+// const arr10 = [[1, 2, 3], [4, 5], [6]];
+
+// let result = arr10
+//     .reduce((acc,item) => {
+//         return acc.concat(item);
+//     }, [])
+//     .reduce((acc,item) => {
+//         return acc += item;
+//     },0)
+
+// console.log(result)
+
+// Task 11
+
+const arr11 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+
+// let sum = arr11.flat(2).reduce((acc,item) => acc += item, 0);
+
+// console.log(sum)
+
+let a = arr11
+    .reduce((acc,item) => {
+        return acc.concat(item);
+    },[])
+    .reduce((acc,item) => {
+        return acc.concat(item);
+    },[])
+    .reduce((acc,item) => {
+        return acc += item;
+    },0)
+
+    console.log(a);
